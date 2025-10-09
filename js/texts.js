67a7318303484ff4f8cb25bd74653a8e584d8e66
admin <a.pobyarzhin@gmail.com>
2025-10-09 13:31:03 +0200

    финал

--- js/texts.js
+++ js/texts.js
@@ -1,7 +1,7 @@
 const TEXTS = {
   ru: {
     task1: `Найди первую букву своего имени и нажми её!<br>Потом нажми <b>Проверить</b> или <b>Enter</b>.`,
-    task2: `Напечатай слово <b>КОТ</b> или <b>CAT</b>.<br>Потом нажми <b>Проверить</b> или <b>Enter</b>.`,
+    task2: `Напечатай слово <b>КОТ</b> </b>.<br>Потом нажми <b>Проверить</b> или <b>Enter</b>.`,
     task3: `Нажимай <b>Пробел</b> — кот шагает вправо. Собери двух котиков. Потом <b>Проверить</b>.`,
     end:   `Все задания выполнены! 🎉 Молодец! Нажми <b>Рестарт</b>, чтобы сыграть снова.`,
 
@@ -13,7 +13,7 @@ const TEXTS = {
   },
   en: {
     task1: `Find the first letter of your name and press it!<br>Then press <b>Check</b> or <b>Enter</b>.`,
-    task2: `Type <b>CAT</b> (or <b>КОТ</b>).<br>Then press <b>Check</b> or <b>Enter</b>.`,
+    task2: `Type <b>CAT</b>.<br>Then press <b>Check</b> or <b>Enter</b>.`,
     task3: `Press <b>Space</b> — the cat walks right. Collect two cats. Then press <b>Check</b>.`,
     end:   `All tasks completed! 🎉 Great job! Press <b>Restart</b> to play again.`,
 
