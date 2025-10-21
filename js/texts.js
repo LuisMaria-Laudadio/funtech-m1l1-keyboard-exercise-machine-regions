26d43a5c7be803974067248de4372dde40733286
LuisMaria-Laudadio <130412817+LuisMaria-Laudadio@users.noreply.github.com>
2025-10-21 11:13:25 -0300

    Text.js cambios

--- js/texts.js
+++ js/texts.js
@@ -1,14 +1,14 @@
 const TEXTS = {
   base: {
-    hintTitle: "Hint",
-    task1: "Find the first letter of your name and press it! Then press <b>Check</b> or <b>Enter</b>.",
-    task2: "Type the word <b>{CAT}</b>. Then press <b>Check</b> or <b>Enter</b>.",
-    task3: "Press <b>Space</b> — make the cat walk forward. Collect 2 cats! 🐾",
-    end: "You completed all tasks, great job! 🎉",
-    finishHint: "All tasks completed! 🎉 Great job! Press <b>Restart</b> to play again.",
-    check: "Check",
-    restart: "Restart",
-    finishTitle: "You completed all tasks, great job!",
+    hintTitle: "Pista",
+    task1: "¡Encuentra la primera letra de tu nombre y presiónala! Luego presiona<b>Verificar</b> or <b>Enter</b>.",
+    task2: "Escribe la palabra <b>{CAT}</b>. Luego presione <b>Verificar</b> or <b>Enter</b>.",
+    task3: "Pulsa la barra espaciadora para que el gato avance. ¡Recoge 2 gatos! 🐾",
+    end: "Completaste todas las tareas, ¡gran trabajo!🎉",
+    finishHint: "¡Todas las tareas completadas! 🎉 ¡Buen trabajo! Pulsa <b>Reiniciar</b> para volver a jugar.",
+    check: "Verificar",
+    restart: "Reinicar",
+    finishTitle: "Completaste todas las tareas, ¡gran trabajo!",
     catName: "Cat", // DO NOT TRANSLATE - FOR ALL REGIONS WE USE THE WORD 'CAT' - in English!
     targetWord: "CAT" // DO NOT TRANSLATE - FOR ALL REGIONS WE USE THE WORD 'CAT' - in English!
   },
